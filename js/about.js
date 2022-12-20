4367e25efec438af2c5acfa459a7c0afa956e3d7
asrorweb <asrorakmalovich2021@gmail.com>
2022-12-20 20:36:16 +0500

    add contact page and hover effects

---
+++ js/about.js
@@ -0,0 +1,11 @@
+// let aboutBack = document.getElementsByClassName(".directors__item--back"),
+//     aboutFront = document.getElementsByClassName(".directors__item--front"),
+//     aboutDirectorsBtn = document.getElementsByClassName(
+//         ".directors__item--button"
+//     );
+
+// aboutDirectorsBtn.addEventListener("click", function () {
+//     aboutBack.classList.add("directors__item--active");
+//     aboutFront.classList.remove("directors__item--active");
+//     console.log(1);
+// });
