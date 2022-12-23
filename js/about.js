f380ca3f3acc48e367e3758a5f8fe4d8f19a897b
asrorweb <asrorakmalovich2021@gmail.com>
2022-12-23 22:57:25 +0500

    add mobile menu and add responsive design

--- js/about.js
+++ js/about.js
@@ -9,3 +9,17 @@
 //     aboutFront.classList.remove("directors__item--active");
 //     console.log(1);
 // });
+
+let MobileMenuOpenBtn = document.querySelector(".mobile__menu--button"),
+    MobileMenuCloseBtn = document.querySelector(".mobile__exit--button"),
+    MobileMenu = document.querySelector(".navigation");
+
+MobileMenuOpenBtn.addEventListener("click", function () {
+    MobileMenu.style.right = "0";
+    console.log(1);
+});
+
+MobileMenuCloseBtn.addEventListener("click", function () {
+    MobileMenu.style.right = "-100%";
+    console.log(1);
+});
